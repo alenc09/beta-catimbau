530bf2b3cdf58c11e46d92f5d247d6d68cc96dad
alenc09 <alenc09@protonmail.com>
2021-11-15 18:13:54 -0300

    Avancei em mais algumas análises e continuei a organização dos scripts

--- scripts/02_analysis_abund.R
+++ scripts/02_analysis_abund.R
@@ -230,3 +230,69 @@ plot(varpart.abund.len.tu)
 
 ### Nestedness component ----
 #not necessary because it was only explained by spatial variables
+
+#Exponential decay models ----
+## data for distance matrixes ----
+dist.LPI<- vegdist(plot_pcnm$LPI, method = "euclidean")
+dist.prec<- vegdist(plot_pcnm$prec, method = "euclidean")
+dist.wei<- vegdist(plot_pcnm$WEI, method = "euclidean")
+dist.gmdi<- vegdist(plot_pcnm$GMDI, method = "euclidean")
+
+dist.LPI.transf<- vegdist(plot_pcnm_transf$LPI, method = "euclidean")
+dist.prec.transf<- vegdist(plot_pcnm_transf$prec, method = "euclidean")
+dist.wei.transf<- vegdist(plot_pcnm_transf$WEI, method = "euclidean")
+dist.gmdi.transf<- vegdist(plot_pcnm_transf$GMDI, method = "euclidean")
+
+## Herbs ----
+
+decay.model(
+  y = 1 - herb.abund.tu,
+  x = dist.prec,
+  model.type = "exponential",
+  y.type = "similarities"
+) -> decay.abund.herb.prec
+
+decay.model(
+  y = 1-  herb.abund.tu,
+  x = dist.LPI,
+  model.type = "exponential",
+  y.type = "similarities"
+) -> decay.abund.herb.lpi
+
+decay.model(
+  y = 1 - herb.abund.tu,
+  x = dist.wei,
+  model.type = "exponential",
+  y.type = "similarities"
+) -> decay.abund.herb.wei
+
+## Wood ----
+decay.model(
+  y = 1 - len.abund.tu,
+  x = dist.prec,
+  model.type = "exponential",
+  y.type = "similarities"
+) -> decay.abund.len.prec
+
+
+decay.model(
+  y = 1 - len.abund.tu,
+  x = dist.LPI,
+  model.type = "exponential",
+  y.type = "similarities"
+) -> decay.abund.len.lpi
+
+
+decay.model(
+  y = len.abund.tu,
+  x = dist.wei,
+  model.type = "exponential",
+  y.type = "dissimilarities"
+) -> decay.abund.len.wei
+
+decay.model(
+  y = len.abund.ne,
+  x = dist.wei,
+  model.type = "exponential",
+  y.type = "dissimilarities"
+) -> decay.abund.len.ne.wei
