b9de9b6a3bcf6b2f9591ab8a51aaa5ea10ce22dd
alenc09 <alencar.lucasc@gmail.com>
2023-01-18 13:51:41 +0000

    initiated the beta-diversity indexes analysis

--- scripts/beta_index.R
+++ scripts/beta_index.R
@@ -9,13 +9,22 @@ library(betapart)
 
 #data----
 read_xlsx(here("data/herbáceas_pp.xlsx"))-> herbs
-read_xlsx(here("data/lenhosas_pp.xlsx"))-> len
+read_xlsx(here("data/lenhosas_pp.xlsx"))-> wood
 
 #analysis----
 ##beta diversity metrics----
+###herbs----
+#### abundance-based dissimilarity indexes----
 herbs %>% 
   mutate(parcela = as.factor(parcela)) %>% 
   rename(total = `Total Geral`) %>% 
   glimpse -> herbs
 
-beta.pair
+decostand(herbs[,-c(1,59)], 'hellinger') -> herbs_hell #First line is the column names and 59 is the total abundance of each site
+
+beta.pair.abund(herbs_hell, index.family = "bray")-> herbs.bray #bray-curtis dissimilarity index
+herbs.bray$beta.bray -> herb.beta.tot #total beta-diversity
+herbs.bray$beta.bray.bal -> herb.beta.tu #turnover share
+herbs.bray$beta.bray.gra -> herb.beta.ne #nestedness share
+
+#### true-diversity indexes----
